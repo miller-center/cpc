1958109ad3e8bb666c560426320d81d28e12c8d0
Matthew Stephens <ms3uf@d-172-25-54-31.bootp.virginia.edu>
2014-04-02 11:06:28 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CpcBlacklight::Application.config.secret_key_base = 'fc92e3c25f3541dc40a0aed4c374ac6111d5e904811a15bcd54b7648b2685522aa969bc958474fc261cb3ed96219ff4e1d305f6d6839c8113531b867867abc30'
