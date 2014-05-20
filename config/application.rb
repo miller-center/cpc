f92f718f01c0fab59907f1568c2be4cdd9bb8848
Matthew Stephens <mattbastard@gmail.com>
2014-05-20 10:12:55 -0400

    for production deployment

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,13 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+if defined?(Bundler)
+    # If you precompile assets before deploying to production, use this line
+    Bundler.require *Rails.groups(:assets => %w(development, test))
+    # If you want your assets lazily compiled in production, use this line
+    #Bundler.require(*Rails.groups)
+end
+
 
 module CpcBlacklight
   class Application < Rails::Application
