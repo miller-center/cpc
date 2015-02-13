5e08985ac7e3e2910051a9e2b79a18e1d93aed8f
Matthew Stephens <mattbastard@gmail.com>
2015-02-13 16:12:11 -0500

    Adding parenthetical notice for restricted URLs
    This notice for users is to advertise that the additional
    url provided may be subscription-only or otherwise access
    controlled.  Very rudimentary implementation for now!

---
+++ app/assets/javascripts/enhanced_source.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+  $('dt.blacklight-alt_source_t').append('*');
+  $('dt.blacklight-alt_source_t').parent().append('<dd class="blacklight-enhanced-note">* (may require payment or login)</dd>');
+});
